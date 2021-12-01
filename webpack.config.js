25bdf15df5590e37b40a2dbb98d187308a123ead
Focusim <RoMMik823@mail.ru>
2021-12-01 12:34:53 +0300

    start project

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = function(env, argv) {
+    const mode = (argv.mode === 'production');
+
+    return {
+        mode: argv.mode,
+        devtool: mode ? '' : 'inline-source-map',
+        entry: {
+            //main: path.resolve(__dirname, './src/index.js'),
+            'template': [
+                './src/index.js',
+                './src/styles/main.scss'
+            ],
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: './js/[name].js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'webpack Boilerplate',
+                template: path.resolve(__dirname, './src/template.html'), // шаблон
+                filename: 'template.html', // название выходного файла
+            }),
+        ],
+        module: {
+            rules: [
+                // JavaScript
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                // изображения
+                {
+                    test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                    type: 'asset/resource',
+                },
+                // шрифты и SVG
+                {
+                    test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                    type: 'asset/inline',
+                },
+                // CSS, PostCSS, Sass
+                {
+                    test: /\.(scss|css)$/,
+                    use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+                },
+            ],
+        }
+    }
+}
+
