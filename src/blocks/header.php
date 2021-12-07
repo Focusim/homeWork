a272236ab931d3df7ada8d67a8a25248c7b850bd
Focusim <RoMMik823@mail.ru>
2021-12-07 19:59:35 +0300

    new

--- src/blocks/header.php
+++ src/blocks/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
     <link rel="stylesheet" href="src/styles/main.scss">
-    <link rel="stylesheet" href="src/styles/index.scss">
+    <link rel="stylesheet" href="index.scss">
 </head>
 <body>
 
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import '../index.scss';
+import './styles/main.scss';
